6223a77fc1f3f6f6756c5e4ec16eeca8bbb27760
RovoMe <rovo@gmx.at>
2013-05-28 02:05:37 +0200

    Updated Unit-Test for DOM generation

--- src/test/java/at/rovo/test/TemplateExtractionText.java
+++ src/test/java/at/rovo/test/TemplateExtractionText.java
@@ -14,67 +14,78 @@ public class TemplateExtractionText
 	public void testDOMGeneration()
 	{
 		String s ="<html><head><title>Dies ist ein Test</title></head><body><p class=\"text\">Erster Test.</p><a href=\"www.test.at\"><img src=\"test.jpg\"/>Anchor Text</a><hr class=\"test\"/><p>Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.</p></body></html>";
-		HTMLNode[] nodes = Parser.getDOMTree(s, false, false, true);
-//		for (HTMLNode node : nodes)
-//		{
-//			if (node.getChildren() != null && node.getChildren().length > 0)
-//			{
-//				System.out.println("No: "+node.getNo());
-//				System.out.println("Name: "+node.getName());
-//				System.out.println("Parent: "+node.getParentNo());
-//			
-//				for (HTMLNode n : node.getChildren())
-//				{
-//					System.out.println("child of "+node.getName()+": "+n.getNo()+" "+n.getName());
-//				}
-//				System.out.println();
-//			}
-//		}
+		HTMLNode[] nodes = Parser.getDOMTree(s);
+		for (HTMLNode node : nodes)
+		{
+			if (node.getChildren() != null && node.getChildren().length > 0)
+			{
+				System.out.println("No: "+node.getNo());
+				System.out.println("Name: "+node.getName());
+				System.out.println("Parent: "+node.getParentNo());
+			
+				for (HTMLNode n : node.getChildren())
+				{
+					System.out.println("child of "+node.getName()+": "+n.getNo()+" "+n.getName());
+				}
+				System.out.println();
+			}
+		}
 		
 		String text0 = "Dies ist ein Test Erster Test. Anchor Text Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.";
 		String text1 = "Dies ist ein Test";
-		String text9 = "Erster Test. Anchor Text Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.";
-		String text19 = "Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.";
+		String text4 = "Erster Test. Anchor Text Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.";
+		String text10 = "Und noch ein Test. Dieser Test wird durch einen weiteren Satz erweitert, der sogar noch einen Nebensatz beinhaltet.";
 
 		String anchorText = "Anchor Text";
 		
 		Assert.assertEquals(text0, nodes[0].getSubtreeText());
 		Assert.assertEquals(text1, nodes[1].getSubtreeText()); 
-		Assert.assertEquals(text9, nodes[9].getSubtreeText());
-		Assert.assertEquals(text19, nodes[19].getSubtreeText());
+		Assert.assertEquals(text4, nodes[4].getSubtreeText());
+		Assert.assertEquals(text10, nodes[10].getSubtreeText());
 		
 		Assert.assertEquals(anchorText, nodes[0].getSubtreeAnchorText());
 		
 		Assert.assertEquals(((double)anchorText.length() / text0.length()), nodes[0].getAnchorTextRatio());
 		// no anchor text inside of node 1
 		Assert.assertEquals(0.0, nodes[1].getAnchorTextRatio());
-		Assert.assertEquals(((double)anchorText.length() / text9.length()), nodes[9].getAnchorTextRatio());
-		// no anchor text inside of node 19
-		Assert.assertEquals(0.0, nodes[19].getAnchorTextRatio());
+		Assert.assertEquals(((double)anchorText.length() / text4.length()), nodes[4].getAnchorTextRatio());
+		// no anchor text inside of node 10
+		Assert.assertEquals(0.0, nodes[10].getAnchorTextRatio());
 		
 		Assert.assertEquals(text0.split(" ").length, nodes[0].getSegNum());
 		Assert.assertEquals(text1.split(" ").length, nodes[1].getSegNum());
-		Assert.assertEquals(text9.split(" ").length, nodes[9].getSegNum());
-		Assert.assertEquals(text19.split(" ").length, nodes[19].getSegNum());
+		Assert.assertEquals(text4.split(" ").length, nodes[4].getSegNum());
+		Assert.assertEquals(text10.split(" ").length, nodes[10].getSegNum());
 		
 		Assert.assertEquals((text0.length() - text0.replaceAll("[,|;|.]", "").length()), nodes[0].getPunctNum());
 		Assert.assertEquals((text1.length() - text1.replaceAll("[,|;|.]", "").length()), nodes[1].getPunctNum());
-		Assert.assertEquals((text9.length() - text9.replaceAll("[,|;|.]", "").length()), nodes[9].getPunctNum());
-		Assert.assertEquals((text19.length() - text19.replaceAll("[,|;|.]", "").length()), nodes[19].getPunctNum());
+		Assert.assertEquals((text4.length() - text4.replaceAll("[,|;|.]", "").length()), nodes[4].getPunctNum());
+		Assert.assertEquals((text10.length() - text10.replaceAll("[,|;|.]", "").length()), nodes[10].getPunctNum());
 	}
 	
 	@Test
 	public void testRealPageDOMCreation() throws IOException
 	{
 		String url = "<html><head><title>Testpage 1</title></head><body><p>This is an example text.</p></html>";
-//		String url = "http://www.washingtonpost.com/business/economy/romney-chose-paul-ryan-to-shift-the-campaign-debate-will-the-gamble-pay-off/2012/08/13/f9ae54e2-e557-11e1-9739-eef99c5fb285_story.html";
-//		String url1 = "<html><head><title>Testpage 1</title></head><body><p>This is an example text.</p></html>";
-//		String url2 = "<html><head><title>Testpage 2</title></head><body><p>This is a further example text.</p><p>It even contains a sentence more.</p></html>";
-
-		
+		String expected = 
+				"<html>\n"+
+						"\t<head>\n"+
+						"\t\t<title>\n"+
+						"\t\t\tTestpage 1 \n"+
+						"\t\t</title>\n"+
+						"\t</head>\n"+
+						"\t<body>\n"+
+						"\t\t<p>\n"+
+						"\t\t\tThis is an example text. \n"+
+						"\t\t</p>\n"+
+						"\t</body>\n"+
+						"</html>";
 		HTMLNode[] nodes = Parser.getDOMTree(url);
+			
+		String output = this.niceHTMLFormat(nodes[0], false);
 		
-		writeFile("output3.html", this.niceHTMLFormat(nodes[0], false));
+//		writeFile("output3.html", output);
+		Assert.assertEquals(expected, output);
 	}
 	
 	private String niceHTMLFormat(HTMLNode node, boolean endTagsIncluded)
@@ -122,6 +133,7 @@ public class TemplateExtractionText
 		return builder.toString();
 	}
 	
+	@SuppressWarnings("unused")
 	private static void writeFile(String file, String content) throws IOException
 	{
 		System.out.print("Writing file '"+file+"'");
